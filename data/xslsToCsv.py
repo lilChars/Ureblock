9a7f66a3a11db392208b4f79bc23c95fafdd965a
c3-CarlosRojas <Carlos.Rojas@c3.ai>
2025-12-17 13:19:04 -0600

    reader mvp

--- data/xslsToCsv.py
+++ data/xslsToCsv.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_excel("UNIDADES DE NEGOCIO 2025.xlsx", dtype=str)
-df.to_csv("UNIDADES DE NEGOCIO 2025.csv", index=False)
+df = pd.read_excel("CATALOGO CUENTAS GRAL.xlsx", dtype=str)
+df.to_csv("CATALOGO CUENTAS GRAL.csv", index=False)
 # Ureblock/src/xslsToCsv.py
